4386e4dddb6dfc89336769caf182ab5d9a26b800
omkarkarande <karande@usc.edu>
2015-09-19 01:54:47 -0700

    Initial Commit

---
+++ nblearn.py
@@ -0,0 +1,13 @@
+import sys
+
+def main():
+
+    #Simple argument consistency check
+    #Exit if arguments not specified
+    if len(sys.argv != 3):
+        print("usage: python3 nblearn.py TRAININGFILE MODELFILE")
+        sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
